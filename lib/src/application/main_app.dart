85b2fa90f15f2f2ca95a8012d1997a56f645b8b1
Habib <habib23me@gmail.com>
2021-02-25 05:28:12 +0300

    added main app

--- lib/src/application/main_app.dart
+++ lib/src/application/main_app.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MainApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello World"),
+        ),
+      ),
+    );
+  }
 }
